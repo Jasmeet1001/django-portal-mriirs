d1c72cf5f0fa0bc3c94249654d83b655ef886b08
Jasmeet <js3393187@gmail.com>
2023-01-08 22:24:34 +0530

    functionality to upload csv files, student column added

--- dashboard/imp_exp.py
+++ dashboard/imp_exp.py
@@ -5,11 +5,15 @@ from .models import ResearchPaper
 def import_excel(file):
     fs = FileSystemStorage()
     filename = fs.save(file.name, file)
-    excel_file = pd.ExcelFile(f'./media/{filename}')
-    col_include = [ 'faculty', 'authors', 'outside author', 'domain', 'title of paper', 'dept.', 'name of journal', 'name of conference', 'title of book', 'title of chapter', 'scholar', 'publication month', 'publication year', 'doi', 'scopus id']
+    if filename.split('.')[1] == 'csv':
+        temp = pd.read_csv(f'./media/{filename}')
+        temp.to_excel(f"./media/{filename.split('.')[0]}.xlsx", index=False)
+        filename = f"{filename.split('.')[0]}.xlsx"
+
+    col_include = [ 'faculty', 'authors', 'outside author', 'domain', 'title of paper', 'dept.', 'name of journal', 'name of conference', 'title of book', 'title of chapter', 'student', 'scholar', 'publication month', 'publication year', 'doi', 'scopus id']
     try:
-        with pd.ExcelFile(f'./media/{filename}') as excel_file:
-            for sheet in excel_file.sheet_names:
+        with pd.ExcelFile(f'./media/{filename}') as file_to_load:
+            for sheet in file_to_load.sheet_names:
                 data = pd.read_excel(f'./media/{filename}', sheet_name=sheet)
                 data_new = data.dropna(how="all").fillna('')[col_include]
                 authors = ''
@@ -19,14 +23,16 @@ def import_excel(file):
                     else:
                         authors = ', '.join([str(rows[2]), str(rows[3])])
 
-                    tbl_val = ResearchPaper.objects.create(faculty=rows[1], authors=f'{authors}', domain=rows[4], title_of_paper=rows[5], dept=rows[6], name_of_journal=rows[7], name_of_conference=rows[8], title_of_book=rows[9], title_of_chapter=rows[10], scholar=rows[11], month=rows[12], year=rows[13])
+                    tbl_val = ResearchPaper.objects.create(faculty=rows[1], authors=f'{authors}', domain=rows[4], title_of_paper=rows[5], dept=rows[6], name_of_journal=rows[7], name_of_conference=rows[8], title_of_book=rows[9], title_of_chapter=rows[10], student=rows[11], scholar=rows[12], month=rows[13], year=rows[14])
 
                     tbl_val.save()
 
         fs.delete(file.name)
+        fs.delete(filename)
         success_message = 'Upload Successful. Details Added.'
         return success_message
     except:
         fs.delete(file.name)
+        fs.delete(filename)
         error_message = 'Upload Failed. Please check content format.'
         return error_message
