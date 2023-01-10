dcd8271c013cfdc1ee58759bb81a74e0c5220235
Jasmeet <js3393187@gmail.com>
2023-01-10 11:21:00 +0530

    added route for template download button

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('paper/new/', views.add_new, name='dashboard-addpaper'),
     path('paper/<int:pk>/edit/', UpdatePaperView.as_view(), name='dashboard-edit'),
     path('paper/export', views.export_data, name='dashboard-export'),
+    path('paper/template-download', views.template_download, name='dashboard-template'),
     path('<str:username>/', views.profile_view, name='dashboard-profile'),
     path('<str:username>/paper/', views.my_papers, name='dashboard-mypaper'),
 
