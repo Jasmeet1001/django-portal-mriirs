4ae69c73437863669527c3b8c23a11503efbcda1
Jasmeet <js3393187@gmail.com>
2023-02-11 17:34:38 +0530

    added functionality to create acc for fac, admin

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,17 +1,17 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
 from .models import Profile, ResearchPaper
 # , AdditionalInfo, current_year
 
 import datetime
 
 class Signup(forms.Form):
+    choices = (
+        ('1', 'Admin'),
+        ('2', 'Faculty')
+    )
+    choice = forms.ChoiceField(choices=choices, label='Create Account For: ')
     email = forms.EmailField(required=True)
-    # class Meta:
-    #     model = User
-    #     fields = ['email']
-        #  'password1', 'password2'
 
 class UserUpdateForm(forms.ModelForm):
     class Meta:
@@ -31,7 +31,7 @@ class AddPaper(forms.ModelForm):
     # year = forms.TypedChoiceField(coerce=int, choices=year_choices, initial=current_year, required=False)
     class Meta:
         model = ResearchPaper
-        fields = [ 'faculty', 'authors', 'domain', 'title_of_paper', 'dept', 'name_of_journal', 'name_of_conference', 'title_of_book', 'title_of_chapter', 'student', 'scholar', 'month', 'year', 'doi', 'index_db']
+        fields = ['faculty', 'authors', 'outside_authors', 'domain', 'title_of_paper', 'dept', 'name_of_journal', 'name_of_conference', 'title_of_book', 'title_of_chapter', 'student', 'scholar', 'month', 'year', 'doi', 'index_db']
         
         labels = {'dept': 'Department', 'index_db': 'Index databse (SCOPUS, SCIE, ESCI, UGC CARE)', 'faculty': 'Faculty (Ex: FET)', 'authors': 'Authors Name', 'domain': 'Domain (Ex: Machine Learning, NLP, etc)', 'student': 'Student (y/n)', 'scholar': 'Scholar (y/n)', 'year': 'Publication Year','month': 'Publication Month'}
     
