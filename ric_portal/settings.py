c14453aff570db157a65567f18ceab65675f0178
Jasmeet <js3393187@gmail.com>
2023-02-14 02:39:57 +0530

    env based debug

--- ric_portal/settings.py
+++ ric_portal/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DJANGO_DEBUG', '') != False
 
 ALLOWED_HOSTS = ['mreicitationexplorer.up.railway.app', '127.0.0.1']
 CSRF_TRUSTED_ORIGINS = ['https://mreicitationexplorer.up.railway.app']
@@ -82,14 +82,11 @@ WSGI_APPLICATION = 'ric_portal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv('NAME'),
-        'USER': os.getenv('USER'),
-        'PASSWORD': os.getenv('PASSWORD'),
-        'HOST': os.getenv('HOST'),
-        'PORT': os.getenv('PORT')
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
